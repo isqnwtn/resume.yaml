c163e8c5a4bc98eea11aae54ef686824fc42f65f
Vismay Raj <vismayraj1@gmail.com>
2023-03-04 14:49:00 +0530

    Added option to render stuff inside square bracket for a tag

--- src/Lib.hs
+++ src/Lib.hs
@@ -13,11 +13,20 @@ class Render a where
 instance Render Text where
   render = id
 
-data Tag a b where
-  T :: a -> b -> Tag a b
-  S :: a      -> Tag a ()
+instance Render () where
+  render _ = ""
 
-data EnclosedList a = EC  [a]
+data Tag a b c where
+  T :: a -> c      -> Tag a () c
+  S :: a           -> Tag a () ()
+  F :: a -> b -> c -> Tag a b c
+instance (Render a,Render b,Render c) => Render (Tag a b c) where
+  render (T name curl   ) = "\\"<>(render name)<>render curl
+  render (S name        ) = "\\"<>(render name)
+  render (F name sq curl) = "\\"<>(render name)<>(render sq)<>(render curl)
+
+
+data EnclosedList a = EC [a]
                     | ES [a]
 instance (Render a) => Render (EnclosedList a) where
   render (EC l) = concat $ map (renderSingle . render ) l
@@ -25,17 +34,14 @@ instance (Render a) => Render (EnclosedList a) where
       renderSingle x = "{" <> x <> "}"
   render (ES l) = "[" <> ( intercalate "," (map render l)  ) <> "]"
 
-instance (Render a,Render b) => Render (Tag a b) where
-  render (T a b) = "\\"<>(render a)<>render b
-  render (S a  ) = "\\"<>(render a)
-
 data Obj a = O Text a
 instance (Render a) => Render (Obj a) where
-  render (O objName body) = unlines
-                       [ render (T "begin" (EC [objName]) :: Tag Text (EnclosedList Text))
-                       , (render body)
-                       , render (T "end" (EC [objName]) :: Tag Text (EnclosedList Text))
-                       ]
+  render (O objName body)
+    = unlines
+    [ render (T "end" (EC [objName]) :: Tag Text () (EnclosedList Text))
+    , render body
+    , render (T "end" (EC [objName]) :: Tag Text () (EnclosedList Text))
+    ]
 
 data Document a = D a
 instance (Render a) => Render (Document a) where
