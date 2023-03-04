58ad55a50adcceaf242aba3bc4edf009340239cb
Vismay Raj <vismayraj1@gmail.com>
2023-03-04 14:33:37 +0530

    Updated Latex types to include new objects

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# haskchain
+# HIARECO
+HIARECO is recursive acronym for `HIARECO Is A REsume COnstructor`, as the name suggest it builds resume from a given simple DSL
+# Installation
+* use `stack build` to build the program
+* install the `texlive-core` package to convert `.tex` files to `.pdf`
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GADTs #-}
 module Lib
     ( someFunc
     ) where
 
-import Prelude hiding (unlines)
-import Data.Text
+import Prelude hiding (unlines,concat)
+import Data.Text hiding (map)
 
 class Render a where
   render:: a -> Text
@@ -12,19 +13,35 @@ class Render a where
 instance Render Text where
   render = id
 
-data Tag a b = T a b
-instance (Render a,Render b) => Render (Tag a b) where
-  render (T a b) = "\\"<>(render a)<>"{"<>render b<>"}"
+data Tag a b where
+  T :: a -> b -> Tag a b
+  S :: a      -> Tag a ()
 
-data Document = D Text
-instance Render Document where
-  render (D body) = unlines
-                  [ render (T "begin" "document":: Tag Text Text)
-                  , body
-                  , render (T "end" "document":: Tag Text Text)
-                  ]
+data EnclosedList a = EC  [a]
+                    | ES [a]
+instance (Render a) => Render (EnclosedList a) where
+  render (EC l) = concat $ map (renderSingle . render ) l
+    where
+      renderSingle x = "{" <> x <> "}"
+  render (ES l) = "[" <> ( intercalate "," (map render l)  ) <> "]"
+
+instance (Render a,Render b) => Render (Tag a b) where
+  render (T a b) = "\\"<>(render a)<>render b
+  render (S a  ) = "\\"<>(render a)
+
+data Obj a = O Text a
+instance (Render a) => Render (Obj a) where
+  render (O objName body) = unlines
+                       [ render (T "begin" (EC [objName]) :: Tag Text (EnclosedList Text))
+                       , (render body)
+                       , render (T "end" (EC [objName]) :: Tag Text (EnclosedList Text))
+                       ]
+
+data Document a = D a
+instance (Render a) => Render (Document a) where
+  render (D body) = render (O "document" body)
 
 
 someFunc :: IO ()
 someFunc = putStrLn $ unpack $
-  render $ D "hello world"
+  render $ (D "hello world" :: Document Text)
