fdb5ddb50f515cea6812ecc4740aedbf226bd844
Vismay Raj <vismayraj1@gmail.com>
2023-03-13 21:37:47 +0530

    Work Experience section, added color

--- src/Gen/Body.hs
+++ src/Gen/Body.hs
@@ -5,10 +5,11 @@ module Gen.Body
 where
 
 import Gen.Common
+import Gen.Work
 import Latex
 import Info
 import Lib
-import Data.Text (unpack,Text)
+import Data.Text (unpack)
 
 resumeBody :: Resume -> Latex Ltx
 resumeBody rsm@Resume{..} =
@@ -31,49 +32,3 @@ tableBody Resume{..} = workExp workExperience
        :#>> (sle $ Str "meow meow")
        :#>> Empty
 
-workExp :: [Experience Work] -> Latex Ltx
-workExp expirience
-  = (sle $ Slash "hline")
-     :#>> (sle $ Slash "section*" :<@> Curl "Work Experience")
-     :#>> (concatLtx $ map singleExp expirience)
-
-expContent :: Experience Work -> Latex Ltx
-expContent e@Experience{..}
-  = (sle $ Slash "bfseries")
-  :#>> (sle $ Str $ unpack $ workPlaceName spec)
-  :#>> (sle $ Slash "newline")
-  :#>> durationPlace
-    where
-      durationPlace = case duration of
-        Nothing -> (sle $ Slash "hfill" :<@> Str ( unpack $ workLocation spec) )
-        Just (Duration s Nothing) ->
-          (sle $ Str (unpack s <> " $\\Rightarrow$ " <> "present" ) )
-          :#>> (sle $ Slash "hfill")
-          :#>> (sle $ Str $ unpack $ workLocation spec)
-        Just (Duration s (Just end)) ->
-          (sle $ Str (unpack s <> " $\\Rightarrow$ " <> unpack end))
-          :#>> (sle $ Slash "hfill")
-          :#>> (sle $ Str $ unpack $ workLocation spec)
-
-achievementsList :: [Text] -> Latex Ltx
-achievementsList l
-  = (Cld (BegEnd "itshape" ELine) :<&>
-     ( Cld (BegEnd "itemize" ELine) :<&>
-     ( concatLtx $ map toItem l)
-     )
-    )
-    where
-      toItem x = sle $ Slash $ "item " <> unpack x
-
-singleExp :: Experience Work -> Latex Ltx
-singleExp e@Experience{..}
-  = (sle $ Str $ "% begin exp" <> (unpack $ workPlaceName spec))
-        :#>> (sle $ Slash "Large")
-        :#>> (sle $ Slash "textbf" :<@> (Curl $ unpack $ position spec))
-        :#>> (sle $ Slash "normalsize")
-        :#>> (sle $ Slash "newline")
-        :#>> (Cld (InCurl (Str "textcolor" :<@> Curl "gray"))
-                :<^> (Cld (BegEnd "slshape" ELine) ) :<&> (expContent e)
-                )
-        :#>> (achievementsList achievements)
-        :#>> (sle $ Str $ "% end exp" <> (unpack $ position spec))
---
+++ src/Gen/Work.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE RecordWildCards #-}
+module Gen.Work
+ ( workExp
+ )where
+
+import Data.Text (unpack,Text)
+import Gen.Common
+import Info
+import Lib
+import Latex
+
+workExp :: [Experience Work] -> Latex Ltx
+workExp expirience
+  = (sle $ Slash "hline")
+     :#>> (sle $ Slash "section*" :<@> Curl "Work Experience")
+     :#>> (concatLtx $ map singleExp expirience)
+
+expContent :: Experience Work -> Latex Ltx
+expContent e@Experience{..}
+  = (sle $ Slash "bfseries")
+  :#>> (sle $ Slash "newline")
+  :#>> durationPlace
+    where
+      durationPlace = case duration of
+        Nothing -> (sle $ Slash "hfill" :<@> Str ( unpack $ workLocation spec) )
+        Just (Duration s Nothing) ->
+          (sle $ Str (unpack s <> " $\\Rightarrow$ " <> "present" ) )
+          :#>> (sle $ Slash "hfill")
+          :#>> (sle $ Str $ unpack $ workLocation spec)
+        Just (Duration s (Just end)) ->
+          (sle $ Str (unpack s <> " $\\Rightarrow$ " <> unpack end))
+          :#>> (sle $ Slash "hfill")
+          :#>> (sle $ Str $ unpack $ workPlaceName spec)
+
+achievementsList :: [Text] -> Latex Ltx
+achievementsList l
+  = (Cld (BegEnd "itshape" ELine) :<&>
+     ( Cld (BegEnd "itemize" ELine) :<&>
+     ( concatLtx $ map toItem l)
+     )
+    )
+    where
+      toItem x = sle $ Slash $ "item " <> unpack x
+
+singleExp :: Experience Work -> Latex Ltx
+singleExp e@Experience{..}
+  = (sle $ Str $ "% begin exp" <> (unpack $ workPlaceName spec))
+        :#>> LX (Opn (DefineColor "DarkBlue" (0.0,0.0,0.3)))
+        :#>> (sle $ Slash "Large")
+        -- position
+        :#>> (sle $ Slash "color" :<@> Curl "DarkBlue")
+        :#>> (sle $ Slash "textbf" :<@> (Curl $ unpack $ position spec))
+        :#>> (sle $ Slash "vspace" :<@> Curl "0.1cm")
+        :#>> (sle $ Slash "newline")
+
+        -- work place name
+        :#>> (sle $ Slash "color" :<@> Curl "darkgray")
+        :#>> (sle $ Slash "large")
+        :#>> (sle $ Str $ unpack $ workPlaceName spec)
+        :#>> (sle $ Slash "normalsize")
+
+        -- duration and place
+        :#>> (Cld (InCurl (Str "textcolor" :<@> Curl "gray"))
+                :<^> (Cld (BegEnd "slshape" ELine) ) :<&> (expContent e)
+                )
+
+        -- achievements
+        :#>> (sle $ Slash "color" :<@> Curl "Black")
+        :#>> (achievementsList achievements)
+        :#>> (sle $ Slash "vspace" :<@> Curl "0.75cm")
+        :#>> (sle $ Str $ "% end exp" <> (unpack $ position spec))
